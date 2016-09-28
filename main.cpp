17a0391e256911d353fec03a967ed6d679c7ac5a
vopi181 <dompace181@gmail.com>
2016-09-28 09:51:53 -0500

    works now

--- main.cpp
+++ main.cpp
@@ -3,39 +3,25 @@
 #include "include/vmem.cpp"
 #include <string>
 #include <errno.h>
-
-
-using namespace std;
-
-
-
-int main() {
-    const char* testing = ("testing.out");
-    vmem m;
-    const char* testingadr = "0x7ffd8031495c";
-
-    pid_t pid;
-    pid = m.getpid(testing);
-    cout << pid << endl;
-    struct iovec local[1];
-    struct iovec remote[1];
-    
-    char* buf1 = new char[8];
-    float nread[2]{0,0};
-
-    local[0].iov_base = buf1;
-    local[0].iov_len = 8;
-    remote[0].iov_base = (void *) 0x7fff0ba2aebc;
-    remote[0].iov_len = 8;
-
-    //nread = process_vm_readv(pid, local, 1, remote, 1, 0);
-    m.read((void*) 0x7fff0ba2aebc, &nread, sizeof(nread));
-    cout << errno << endl;
-    cout << nread << endl;
-    //cout << buf1 << endl;
-
-
-
-
-
+#include <sys/ptrace.h>
+int main()
+{
+vmem m;
+pid_t pid;
+pid = m.getpid("testing.out");
+cout << pid << endl;
+auto addr = 0x7ffcf5396c98;
+long ret = ptrace(PTRACE_ATTACH, pid, NULL, NULL);
+cout << "ptrace Status: " << ret << endl;
+cout << "Errno: " << errno << endl;
+ret = ptrace(PTRACE_TRACEME, pid, NULL, NULL);
+cout << "ptrace Status: " << ret << endl;
+cout << "Errno: " << errno << endl;
+ret = ptrace(PTRACE_PEEKDATA, pid, addr, NULL);
+cout << "ptrace Status: " << ret << endl;
+cout << "Errno: " << errno << endl;
+ret = ptrace(PTRACE_DETACH, pid, NULL, NULL);
+cout << "ptrace Status: " << ret << endl;
+cout << "Errno: " << errno << endl;
+return 0;
 }
--- testing/main.cpp
+++ testing/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 #include <stdio.h>
+#include <unistd.h>
+
 int main() {
-    int test;
-    test = 123;
-    std::cout << test << std::endl;
+    int test = 123;
+    int* iptr = &test;
+    auto x = iptr;
+    std::cout << *x << std::endl;
     std::cout << &test << std::endl;
     getchar();
+    std::cout << *x << std::endl;
+    std::cout << &test << std::endl;
+    getchar();
+    int z = 1;
+    while(z == 1) {
+    std::cout << *x << std::endl;
+    std::cout << &test << std::endl;
+    usleep(3000000);
+    }
+    return 0;
 
 }
